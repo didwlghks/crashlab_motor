4131701a29d10dadf45913ac39c540535c8ae973
didwlghks <66863060+didwlghks@users.noreply.github.com>
2021-11-25 00:48:44 +0900

    Update main.cpp

--- motor_pkg/src/main.cpp
+++ motor_pkg/src/main.cpp
@@ -1,3 +1,31 @@
+#include <ros/ros.h>
+#include <motor_test/motor_node.h>
+#include <motor_test/pid.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "motor_node");
+  ros::NodeHandle nh;
+  Initialize();
+ ros::Rate loop_rate(Control_cycle);
+
+  while(ros::ok())
+  {
+    //Motor_Controller(1, true, 100);
+    //Motor_Controller(2, true, 100);
+    //Accel_Controller(1, true, 100);
+    //Accel_Controller(2, true, 100);
+    //Switch_Turn_Example(100, 100);
+    //Theta_Distance(180,100,30,110);
+    Motor_View();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  Motor_Controller(1, true, 0);
+  Motor_Controller(2, true, 0);
+  return 0;
+}
 /*
 int state = 0;
 //0 : 홍보 종료상태
@@ -42,56 +70,3 @@ break;
 
 홍보 과정 종료시 신호받아서 state = 0으로 초기화해줘야
   */
-}
-#include <ros/ros.h>
-#include <motor_test/motor_node.h>
-#include <motor_test/pid.h>
-#include <stdio.h>
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "motor_node");
-  ros::NodeHandle nh;
-  Initialize();
- ros::Rate loop_rate(Control_cycle);
-
-  while(ros::ok())
-  {
-    //Motor_Controller(1, true, 100);
-    //Motor_Controller(2, true, 100);
-    //Accel_Controller(1, true, 100);
-    //Accel_Controller(2, true, 100);
-    //Switch_Turn_Example(100, 100);
-    //Theta_Distance(180,100,30,110);
-    Motor_View();
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  Motor_Controller(1, true, 0);
-  Motor_Controller(2, true, 0);
-  return 0;
-}
-
-/* //PID EXAMPLE
-int main() {
-
-        // Kp -  proportional gain
-        // Ki -  Integral gain
-        // Kd -  derivative gain
-        // dt -  loop interval time
-        // max - maximum value of manipulated variable
-        // min - minimum value of manipulated variable
-
-    PID pid = PID(0.1, 100, -100, 0.1, 0.5, 0.01); //dt, max, min, kp, ki, kd
-                                                   
-
-    double val = 20;
-    for (int i = 0; i < 100; i++) {
-        double inc = pid.calculate(0, val);
-        printf("val:% 7.3f inc:% 7.3f\n", val, inc);
-        val += inc;
-    }
-
-    return 0;
-}
-*/
