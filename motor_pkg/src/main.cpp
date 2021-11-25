43cca8d7d5d96dd986d73a7d8c8325a9e56fc702
didwlghks <66863060+didwlghks@users.noreply.github.com>
2021-11-25 14:19:59 +0900

    Update main.cpp

--- motor_pkg/src/main.cpp
+++ motor_pkg/src/main.cpp
@@ -3,7 +3,7 @@
 #include <motor_test/motor_node.h>
 #include <my_msgs/CameraData.h>
 #include <my_msgs/SensorData.h>
-#include <my_msgs/Signal.h>
+#include <my_msgs/SignalData.h>
 #include <stdio.h>
 
 #define X_CENTER_1 -100
@@ -35,7 +35,7 @@ void SensorDataCallback(const my_msgs::SensorData &msg){
 }
 
 void SignalDataCallback(const my_msgs::SignalData &msg){
-    signal = msg.signal;
+    signal = msg.data;
 }
 
 
@@ -44,19 +44,19 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "motor_node");
   ros::NodeHandle nh;
   Initialize();
-  ros::Publisher signal_pub = nh.advertise<my_msgs::SignalData>("/signal/topic",10); // give signal to Display
+  ros::Publisher signal_pub = nh.advertise<my_msgs::SignalData>("/signal/topic",10);
   ros::Rate loop_rate(Control_cycle);
-  my_msgs::Signal signal;
+  my_msgs::SignalData data;
 
   while(ros::ok())
   {
-    ros::Subscriber sensor_sub = nh.subscribe("/sensor/topic", 10, SensorDataCallback);
+    ros::Subscriber sensor_sub = nh.subscribe("/sensor/topic", 10, SensorDataCallback); //sensor topic sub to protect robot
       
     if(sequence == 1)
     {
         ros::Subscriber camera_sub = nh.subscribe("/camera/topic", 10, CamDataCallback); //camera topic sub
     
-        Motor_Controller(1, true, 50);
+        Motor_Controller(1, true, 50); //go foward normally
         Motor_Controller(2, true, 50);
     
         if(x_point <= X_CENTER_1)
@@ -75,7 +75,8 @@ int main(int argc, char** argv)
             Motor_Controller(2, true, 0);
             if(FrontSensor < SENSOR_LIMIT)
             {
-                pub_number.publish(signal);
+                data = 1; //publish SignalData.data to display
+                signal_pub.publish(data);
                 sequence = 2;
                 break;
             }
@@ -84,7 +85,7 @@ int main(int argc, char** argv)
       
     else if(sequence == 2)
     {
-        ros::Subscriber sensor_sub = nh.subscribe("/sensor/topic", 10, SensorDataCallback);
+        ros::Subscriber signal_sub = nh.subscribe("/signal/topic", 10, SignalDataCallback); //
         if(signal == 1) //end signal input
         {
             sequence = 1;
